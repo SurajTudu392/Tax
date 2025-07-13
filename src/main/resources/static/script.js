8801dd84a05b162498239add05c177f93210d81b
UpasanaBanik <upasanabanik14@gmail.com>
2025-07-13 13:03:02 +0530

    TUM BURBAK HO

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -5,3 +5,25 @@ function getDate()
     x.innerHTML=curr_date.toString();
 }
 window.onload=getDate;
+function Bill()
+{
+    var gst=document.getElementById("tax").value;
+    document.getElementById("tax").innerHTML = "You typed: " + gst;
+    var s="";
+    for(let i=0;i<=gst.length;i++)
+    {
+        if(gst.charAt(i)!='%'&&gst.charAt(i)!=' ')
+        {
+            s=s+gst.charAt(i);
+
+    }
+}
+var Gst=parseInt(gst);
+    Gst=Gst/100.0;
+var b=document.getElementById("am").value;
+var Price=parseInt(b);    
+    var Total=Gst+Price;
+    document.getElementById("t1").innerHTML=" "+Total;
+
+
+}
